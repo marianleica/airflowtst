d5db77bfab38606360b6cbb00d4a5cd0296de670
Marian Leica <maleica@microsoft.com>
2025-10-07 22:01:15 +0300

    Update tinatic2-dag.py

--- dags/tinatic2-dag.py
+++ dags/tinatic2-dag.py
@@ -9,56 +9,52 @@ default_args = {
     'start_date': datetime(2024, 2, 12)
 }
 
+@dag(
+    default_args=default_args,
+    schedule_interval="@once",
+    description="Simple Pipeline with Titanic",
+    catchup=False,
+    tags=['Titanic']
+)
 def titanic_processing():
 
-    @dag(
-        default_args=default_args,
-        schedule_interval="@once",
-        description="Simple Pipeline with Titanic",
-        catchup=False,
-        tags=['Titanic']
+    start = EmptyOperator(task_id='start')
+
+    @task
+    def first_task():
+        print("And so, it begins!")
+
+    @task
+    def read_data():
+        df = pd.read_csv("https://raw.githubusercontent.com/donhighmsft/airflowexamples/main/dag_data/titantic.csv", sep=";")
+        survivors = df.loc[df.Survived == 1, "Survived"].sum()
+        survivors_sex = df.loc[df.Survived == 1, ["Survived", "Sex"]].groupby("Sex").count()
+        return {
+            'survivors_count': int(survivors),
+            'survivors_sex': survivors_sex.to_dict()
+        }
+
+    @task
+    def print_survivors(source):
+        print(f"Total survivors: {source['survivors_count']}")
+
+    @task
+    def print_survivors_by_sex(source):
+        print(f"Survivors by sex: {source['survivors_sex']}")
+
+    last = BashOperator(
+        task_id="last_task",
+        bash_command='echo "This is the last task performed with Bash."',
     )
-    def _dag():
 
-        start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id='end')
 
-        @task
-        def first_task():
-            print("And so, it begins!")
+    first = first_task()
+    downloaded = read_data()
+    start >> first >> downloaded
+    surv_count = print_survivors(downloaded)
+    surv_sex = print_survivors_by_sex(downloaded)
 
-        @task
-        def read_data():
-            df = pd.read_csv("https://raw.githubusercontent.com/donhighmsft/airflowexamples/main/dag_data/titantic.csv", sep=";")
-            survivors = df.loc[df.Survived == 1, "Survived"].sum()
-            survivors_sex = df.loc[df.Survived == 1, ["Survived", "Sex"]].groupby("Sex").count()
-            return {
-                'survivors_count': survivors,
-                'survivors_sex': survivors_sex.to_dict()
-            }
+    [surv_count, surv_sex] >> last >> end
 
-        @task
-        def print_survivors(source):
-            print(source['survivors_count'])
-
-        @task
-        def survivors_sex(source):
-            print(source['survivors_sex'])
-
-        last = BashOperator(
-            task_id="last_task",
-            bash_command='echo "This is the last task performed with Bash."',
-        )
-
-        end = EmptyOperator(task_id='end')
-
-        first = first_task()
-        downloaded = read_data()
-        start >> first >> downloaded
-        surv_count = print_survivors(downloaded)
-        surv_sex = survivors_sex(downloaded)
-
-        [surv_count, surv_sex] >> last >> end
-
-    return _dag()
-
-execution = titanic_processing()
+dag_instance = titanic_processing()
