1b3e4c1d04f9ef5e557050a7bb5115f53f5f41e3
Marian Leica <maleica@microsoft.com>
2025-10-07 21:57:13 +0300

    Update tinatic2-dag.py

--- dags/tinatic2-dag.py
+++ dags/tinatic2-dag.py
@@ -11,7 +11,7 @@ default_args = {
 
 @dag(
     default_args=default_args,
-    schedule_interval="@once",
+    schedule_internal="@once",  # ✅ correct spelling
     description="Simple Pipeline with Titanic",
     catchup=False,
     tags=['Titanic']
